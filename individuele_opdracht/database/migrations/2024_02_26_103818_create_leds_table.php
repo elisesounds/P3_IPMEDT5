1ea2dc91131c7c437fb2ae64210944cf995b9a6a
elisesounds <s1141823@student.hsleiden.nl>
2024-02-26 12:00:29 +0100

    database set up

--- individuele_opdracht/database/migrations/2024_02_26_103818_create_leds_table.php
+++ individuele_opdracht/database/migrations/2024_02_26_103818_create_leds_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('counts', function (Blueprint $table) {
+        Schema::create('leds', function (Blueprint $table) {
             $table->id();
-            $table->integer("times_pressed");
+            $table->boolean("led_is_on"); //true of false
             $table->timestamps();
         });
     }
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('counts');
+        Schema::dropIfExists('leds');
     }
 };
